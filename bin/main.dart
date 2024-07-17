a7f368daea9ec613211da4e9f3973940a79d4ab0
atsalrizandri <atsalrizandrii@gmail.com>
2024-07-17 19:34:28 +0700

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,4 @@
+void main(){
+  var name = ("Muhammad Atsal Rizandri");
+  print("Hello, I\'m $name and i\'m learning dart programming language");
+}
---
+++ test/temperature_conversion_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:temperature_conversion_project/temperature_conversion_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
